6591812d9f4a02acc3004a1ad78b7d85b9fad352
ShenJiaoBen <1874787268@qq.com>
2025-05-03 08:07:25 +0800

    加载器

---
+++ 2.lua
@@ -0,0 +1,26 @@
+local Games = {
+  [3623096087] = [[
+KingScript = "By LN" 
+Roblox= "霖溺Script"
+KingTeam= "LegendofPower" 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/LN%E5%8A%9B%E9%87%8F.lua"))()
+]], -- 力量传奇
+
+  [16732694052] = [[
+LnScript = "By LN" 
+Roblox= "Fish"
+LnTeam= "免费" 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/FishScriptLn.lua"))()
+]], -- Fisch
+
+  [7239319209] = [[
+KingScript = "By King" Roblox= "草拟吗"
+KingTeam= "King无敌" 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ShenJiaoBen/ShenJiaoBen/refs/heads/main/new_ohio.lua"))()
+]],--ohio
+
+
+  
+}
+
+return Games
